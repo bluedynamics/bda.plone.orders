989b7159e7bff9a6f7ad16de66c22de23d00fd3a
Johannes Raggam <raggam-nl@adm.at>
2014-01-24 13:06:56 +0100

    use logging infrastructure, reindex soup

--- src/bda/plone/orders/upgrades.py
+++ src/bda/plone/orders/upgrades.py
@@ -1,9 +1,13 @@
-import uuid
+from bda.plone.orders.common import get_vendor
+from plone.app.uuid.utils import uuidToObject
+from plone.uuid.interfaces import IUUID
 from souper.soup import get_soup
 from zope.component.hooks import getSite
-from plone.uuid.interfaces import IUUID
-from plone.app.uuid.utils import uuidToObject
-from bda.plone.orders.common import get_vendor
+
+import logging
+import uuid
+
+logger = logging.getLogger('bda.plone.orders UPGRADE')
 
 
 def fix_bookings_shop_uid(ctx=None):
@@ -12,6 +16,7 @@ def fix_bookings_shop_uid(ctx=None):
     portal = getSite()
     soup = get_soup('bda_plone_orders_bookings', portal)
     data = soup.storage.data
+    need_reindex = False
     for item in data.values():
         if not 'shop_uid' in item.attrs\
                 or not isinstance(item.attrs['shop_uid'], uuid.UUID):
@@ -20,5 +25,10 @@ def fix_bookings_shop_uid(ctx=None):
             shop = get_vendor(obj)
             shop_uid = uuid.UUID(IUUID(shop))
             item.attrs['shop_uid'] = shop_uid
-            print("ADDED shop_uid TO BOOKING %s" % item.attrs['uid'])
-    # XXX: rebuild soup catalog and reindex
+            need_reindex = True
+            logging.info(
+                "Added shop_uid to booking {0}".format(item.attrs['uid'])
+            )
+    if need_reindex:
+        soup.reindex()
+        logging.info("Reindexed bookings souper storage")
