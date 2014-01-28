b9de2ee0dae9aac41d2fbf282d34f2dd0ea14809
Johannes Raggam <raggam-nl@adm.at>
2014-01-28 15:33:04 +0100

    change to yafowil compatible vocabulary

--- src/bda/plone/orders/vocabularies.py
+++ src/bda/plone/orders/vocabularies.py
@@ -1,17 +1,10 @@
 from bda.plone.orders.common import get_all_vendors
 from plone.uuid.interfaces import IUUID
-from zope.interface import directlyProvides
-from zope.schema.interfaces import IVocabularyFactory
-from zope.schema.vocabulary import SimpleTerm
-from zope.schema.vocabulary import SimpleVocabulary
 
 
-def AllVendorAreas(context, query=None):
+def all_vendors_vocab():
     """Vocabulary for all vendor areas by uuid.
     """
     all_vendors = get_all_vendors()
-    tz_list = [SimpleTerm(value=IUUID(it), title=it.Title())
-               for it in all_vendors
-               if query is None or query.lower() in it.lower()]
-    return SimpleVocabulary(tz_list)
-directlyProvides(AllVendorAreas, IVocabularyFactory)
+    vocab = [(IUUID(it), it.Title()) for it in all_vendors]
+    return vocab
