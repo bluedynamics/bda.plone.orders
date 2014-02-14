43520c83cfe92ef8ef6ca66d02e1cc4fda428862
Jens W. Klein <jens@bluedynamics.com>
2014-02-14 14:05:05 +0100

    works basically

--- src/bda/plone/orders/browser/mailtemplates.py
+++ src/bda/plone/orders/browser/mailtemplates.py
@@ -5,6 +5,7 @@ from bda.plone.ajax import ajax_continue
 from bda.plone.ajax import ajax_form_fiddle
 from bda.plone.orders import message_factory as _
 from node.utils import UNSET
+from yafowil.base import ExtractionError
 from yafowil.plone.form import YAMLForm
 
 
@@ -14,8 +15,11 @@ class MailtemplatesForm(YAMLForm):
     form_template = 'bda.plone.orders.browser:forms/mailtemplates.yaml'
     message_factory = _
 
-    def value_templates(self, widget, data):
+    def value_tpl(self, widget, data):
         return UNSET
+
+    def validate_tpl(self, widget, data):
+        raise ExtractionError('fooo')
 
     def form_action(self, widget, data):
         return '%s/@@mailtemplates' % self.context.absolute_url()
