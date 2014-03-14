3e5abc7f4a0279e699498e096001085e2c5c23d8
Robert Niederreiter <office@squarewave.at>
2014-03-14 21:48:48 +0100

    add required_interface

--- src/bda/plone/orders/localroles.py
+++ src/bda/plone/orders/localroles.py
@@ -2,9 +2,11 @@ from zope.interface import implementer
 from plone.app.workflow.interfaces import ISharingPageRole
 from bda.plone.orders import permissions
 from bda.plone.orders import message_factory as _
+from bda.plone.orders.interfaces import IVendor
 
 
 @implementer(ISharingPageRole)
 class VendorRole(object):
     title = _(u"title_is_vendor", default=u"Is Vendor")
     required_permission = permissions.DelegateVendorRole
+    required_interface = IVendor
