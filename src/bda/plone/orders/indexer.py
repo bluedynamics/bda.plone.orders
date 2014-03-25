23dcba9926aa01ba9d1e124e38fd75ef73849269
Johannes Raggam <raggam-nl@adm.at>
2014-03-25 15:10:16 +0100

    construct list of users and groups with customer roles directly on context

--- src/bda/plone/orders/indexer.py
+++ src/bda/plone/orders/indexer.py
@@ -1,5 +1,6 @@
 from zope.interface import Interface
 from plone.indexer import indexer
+import plone.api
 
 
 @indexer(Interface)
@@ -7,4 +8,8 @@ def customer_role(obj):
     # read users and groups having customer role directly on context, not
     # inherited! groups gets prefixed with ``group:``
     # XXX
-    return []
+
+    # TODO: seems not to be called on reindexObject
+    users = obj.users_with_local_role('Customer')  # get non-aquired roles
+    ret = [plone.api.group.get(it) and 'group:%s' or it for it in users]
+    return ret
