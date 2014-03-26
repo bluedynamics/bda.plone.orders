dd1c1df6deb082ea7539e2036e68789ed38b39b4
Johannes Raggam <raggam-nl@adm.at>
2014-03-26 15:15:56 +0100

    ZCatalog/catalog_object does the same, if not passed

--- src/bda/plone/orders/subscriber.py
+++ src/bda/plone/orders/subscriber.py
@@ -7,5 +7,4 @@ def reindex_customer_role(context, event):
     catalog = api.portal.get_tool(name='portal_catalog')
     catalog.reindexObject(context,
                           idxs=['customer_role'],
-                          update_metadata=0,
-                          uid='/'.join(context.getPhysicalPath()))
+                          update_metadata=0)
