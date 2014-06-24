39d7a0471e98390abbba234566a3940620705e38
Johannes Raggam <raggam-nl@adm.at>
2014-06-25 01:21:09 +0200

    Add ``buyable_uid`` export attribute to CSV exports.

--- src/bda/plone/orders/browser/views.py
+++ src/bda/plone/orders/browser/views.py
@@ -973,7 +973,15 @@ def resolve_buyable_url(context, booking):
         return obj.absolute_url()
     return None
 
+
+def buyable_uid(context, booking):
+    obj = get_object_by_uid(context, booking.attrs['buyable_uid'])
+    if obj:
+        return IUUID(obj)
+    return None
+
 COMPUTED_BOOKING_EXPORT_ATTRS['url'] = resolve_buyable_url
+COMPUTED_BOOKING_EXPORT_ATTRS['buyable_uid'] = buyable_uid
 
 
 def cleanup_for_csv(value):
