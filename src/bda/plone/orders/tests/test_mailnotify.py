f6e249197c2864de569ec8bdc1080010d4425354
Johannes Raggam <raggam-nl@adm.at>
2015-06-27 13:35:42 +0200

    add tests for last changes

---
+++ src/bda/plone/orders/tests/test_mailnotify.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from bda.plone.orders import mailnotify as MN
+import unittest2 as unittest
+
+
+class TestMailnotifyUnit(unittest.TestCase):
+
+    def test_indent_wrap(self):
+        """The _indent mehtod should wrap like defined by it's parameters.
+        """
+        txt = u"abcd " * 3
+        ctrl = '     abcd\nabcd abcd'  # textwrap removes whitespace at end of lines  # noqa
+        res = MN._indent(txt, width=10, ind=5)
+        self.assertEqual(res, ctrl)
+
+    def test_indent_unicode(self):
+        """The _indent mehtod should be able to handle non-ASCII data.
+        """
+        txt = 'äüöß ' * 3
+        ctrl = 'äüöß äüöß\näüöß'  # textwrap removes whitespace at end of lines
+        res = MN._indent(txt, width=10, ind=0)
+        self.assertEqual(res, ctrl)
