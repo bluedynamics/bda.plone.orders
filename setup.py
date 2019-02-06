83da3f531bb190d7b0d6d898327b9bb1607e189a
Peter Holzer <peter.holzer@agitator.com>
2019-02-06 15:30:53 +0100

    update version and classifiers - 2.x targets Plone 5.1/5.2 without Archetypes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 import os
 
 
-version = '1.0a1.dev0'
+version = '2.0.dev0'
 shortdesc = "Orders persistence and backoffice UI for bda.plone.shop"
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'CHANGES.rst')).read()
@@ -18,11 +18,11 @@ setup(
     description=shortdesc,
     long_description=longdesc,
     classifiers=[
-        'Environment :: Web Environment',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.1",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
     ],
     author='BlueDynamics Alliance',
     author_email='dev@bluedynamics.com',
