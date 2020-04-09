6af707d6aac285be1cd3651d1b7dec3e13aac103
Jens W. Klein <jk@kleinundpartner.at>
2020-04-09 17:17:33 +0200

    add path to serialized booking

--- src/bda/plone/orders/restapi/serializer.py
+++ src/bda/plone/orders/restapi/serializer.py
@@ -3,6 +3,7 @@ from bda.plone.cart.restapi.converter import json_compatible_dict
 from bda.plone.orders.datamanagers.booking import BookingData
 from bda.plone.orders.interfaces.orders import IBookingData
 from bda.plone.orders.interfaces.orders import IOrderData
+from plone import api
 from plone.restapi.interfaces import ISerializeToJson
 from zope.component import adapter
 from zope.component import getMultiAdapter
@@ -13,31 +14,39 @@ from zope.publisher.interfaces import IRequest
 @implementer(ISerializeToJson)
 @adapter(IBookingData, IRequest)
 class SerializeBookingToJson(object):
-    def __init__(self, booking, request):
-        self.booking = booking
+    def __init__(self, booking_dm, request):
+        self.booking_dm = booking_dm
         self.request = request
 
     def __call__(self):
-        return json_compatible_dict(dict(self.booking.booking.attrs))
+        data = dict(self.booking_dm.booking.attrs)
+        try:
+            obj = api.content.get(
+                UID=self.booking_dm.booking.attrs['buyable_uid'],
+            )
+            data['path'] = "/".join(obj.getPhysicalPath())
+        except ValueError:
+            data['path'] = None
+        return json_compatible_dict(data)
 
 
 @implementer(ISerializeToJson)
 @adapter(IOrderData, IRequest)
 class SerializeOrderToJson(object):
-    def __init__(self, order, request):
-        self.order = order
+    def __init__(self, order_dm, request):
+        self.order_dm = order_dm
         self.request = request
 
     def __call__(self):
         result = {
-            "order": json_compatible_dict(dict(self.order.order.attrs)),
+            "order": json_compatible_dict(dict(self.order_dm.order.attrs)),
             "bookings": [],
         }
-        for booking_record in self.order.bookings:
+        for booking_record in self.order_dm.bookings:
             serializer = getMultiAdapter(
                 (
                     BookingData(
-                        self.order.context, booking=booking_record, vendor_uids=[]
+                        self.order_dm.context, booking=booking_record, vendor_uids=[]
                     ),
                     self.request,
                 ),
